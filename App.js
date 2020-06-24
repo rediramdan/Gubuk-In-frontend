da121dfb66639c4b6c7439976741e788545edae0
rediramdan <redirpl2smkn1kawali2@gmail.com>
2020-06-24 08:43:05 +0700

    Add-Home-Page

---
+++ App.js
@@ -0,0 +1,85 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {YellowBox} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Text} from 'react-native';
+
+import Home from './src/screens/Home';
+
+const Tab = createBottomTabNavigator();
+const {Navigator, Screen} = createStackNavigator();
+const BottomTab = () => {
+  const style = {style: {paddingBottom: 5, paddingTop: 5}};
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      shifting={true}
+      tabBarOptions={style}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: ({focused}) => {
+            const color = focused ? '#5E94FF' : 'rgba(0,0,0,0.2)';
+            const size = 10;
+            return <Text style={{color: color, fontSize: size}}>Home</Text>;
+          },
+          tabBarIcon: ({focused}) => {
+            const color = focused ? '#5E94FF' : 'rgba(0,0,0,0.2)';
+            return <Icon style={{color: color, fontSize: 32}} name="home" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="BukuSaya"
+        component={Home}
+        options={{
+          tabBarLabel: ({focused}) => {
+            const color = focused ? '#5E94FF' : 'rgba(0,0,0,0.2)';
+            const size = 10;
+            return <Text style={{color: color, fontSize: size}}>Beranda</Text>;
+          },
+          tabBarIcon: ({focused}) => {
+            const color = focused ? '#5E94FF' : 'rgba(0,0,0,0.2)';
+            return <Icon style={{color: color, fontSize: 25}} name="book-open" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarLabel: ({focused}) => {
+            const color = focused ? '#5E94FF' : 'rgba(0,0,0,0.2)';
+            const size = 10;
+            return <Text style={{color: color, fontSize: size}}>Buku saya</Text>;
+          },
+          tabBarIcon: ({focused}) => {
+            const color = focused ? '#5E94FF' : 'rgba(0,0,0,0.2)';
+            return <Icon style={{color: color, fontSize: 25}} name="book" />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home">
+        <Screen name="Home" component={BottomTab} />
+      </Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,310 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Content,
+  Input,
+  TabHeading,
+  Title,
+  CardItem,
+  Item,
+  Tabs,
+  Tab,
+  ScrollableTab,
+  Text,
+  Thumbnail,
+} from 'native-base';
+import {StyleSheet, View, Dimensions, TouchableOpacity} from 'react-native';
+import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
+import {FlatList} from 'react-native-gesture-handler';
+import {Rating, AirbnbRating} from 'react-native-ratings';
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+  brand: {
+    marginLeft: -25,
+  },
+  cPrimary: {
+    color: '#2CA8FF',
+  },
+  header: {
+    backgroundColor: '#2469EF',
+  },
+  banner: {
+    flex: 1,
+    paddingTop: 20,
+    paddingHorizontal: 25,
+    backgroundColor: '#5E94FF',
+    minHeight: 170,
+  },
+  textBanner1: {
+    fontSize: 18,
+    color: 'white',
+  },
+  textBanner: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  avatarBanner: {
+    position: 'absolute',
+    top: 5,
+    right: 15,
+    height: 130,
+    width: 205,
+    borderRadius: 0,
+  },
+  carditem: {
+    paddingTop: 5,
+  },
+  left: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0,0,0,0.1)',
+    paddingBottom: 10,
+  },
+  thumb: {
+    width: 90,
+    height: 130,
+    borderRadius: 5,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+});
+
+const Home = () => {
+  return (
+    <Container>
+      <Header style={styles.header} androidStatusBarColor={'#2469EF'}>
+          <Left style={{paddingLeft:15}}>
+              <IconM name="bookmark-multiple" size={24} color={'white'} />
+          </Left>
+        <Body>
+          <Title style={styles.brand}>Gubuk-In</Title>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Thumbnail
+              style={{width: 40, height: 40}}
+              source={{
+                uri: 'https://placeimg.com/140/140/any',
+              }}
+            />
+          </Button>
+        </Right>
+      </Header>
+      <Content>
+        <View style={styles.banner}>
+          <Text style={styles.textBanner1}>Selamat datang</Text>
+          <Text style={styles.textBanner}>Redi ramdan</Text>
+          <Thumbnail
+            style={styles.avatarBanner}
+            source={require('../images/bg.png')}
+          />
+          <View style={{marginTop: 25}}>
+            <Item
+              style={{
+                backgroundColor: 'rgba(255,255,255,0.9)',
+                borderRadius: 5,
+                paddingHorizontal: 20,
+                height: 44,
+              }}>
+              <Input
+                style={{fontSize: 15}}
+                placeholder="Cari buku "
+                placeholderTextColor={'rgba(0,0,0,0.4)'}
+              />
+            </Item>
+          </View>
+        </View>
+        <View
+          style={{
+            marginTop: -15,
+            backgroundColor: 'white',
+            paddingTop: 15,
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+          }}>
+          <Text style={{marginLeft: 23, marginBottom: 10}}>Terbaru</Text>
+          <FlatList
+            style={{width: width, paddingHorizontal: 15}}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={[1, 2, 3, 4]}
+            renderItem={({item}) => (
+              <View
+                style={{
+                  width: 110,
+                  height: 160,
+                  backgroundColor: 'rgba(0,0,0,0.3)',
+                  borderRadius: 5,
+                  marginRight: 15,
+                }}
+              />
+            )}
+            keyExtractor={(item) => item.toString()}
+          />
+        </View>
+        <View>
+          <Tabs
+            style={{marginTop: 20}}
+            tabBarUnderlineStyle={{backgroundColor: '#5E94FF'}}
+            springFriction>
+            <Tab
+              heading="Gratis"
+              tabStyle={{backgroundColor: 'white'}}
+              activeTabStyle={{backgroundColor: 'white'}}
+              textStyle={{color: 'black'}}
+              activeTextStyle={{color: 'black'}}>
+              <View style={{paddingTop: 20}}>
+                <TouchableOpacity onPress={() => {}}>
+                  <CardItem style={styles.carditem}>
+                    <Left style={styles.left}>
+                      <Thumbnail
+                        square
+                        style={styles.thumb}
+                        source={{
+                          uri: 'https://placeimg.com/140/140/any',
+                        }}
+                      />
+                      <Body>
+                        <Text numberOfLines={2}>
+                          Ilmu Pengetahuan Alam kelas XII SMA
+                        </Text>
+                        <Text note numberOfLines={1}>
+                          Redi ramdan
+                        </Text>
+                        <Text note>Ilmu pengetahuan</Text>
+                        <View style={{alignItems: 'flex-start'}}>
+                          <Rating
+                            readonly={true}
+                            ratingCount={5}
+                            startingValue={3.5}
+                            showRating={false}
+                            imageSize={20}
+                            style={{paddingVertical: 10}}
+                          />
+                        </View>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                  <CardItem style={styles.carditem}>
+                    <Left style={styles.left}>
+                      <Thumbnail
+                        square
+                        style={styles.thumb}
+                        source={{
+                          uri: 'https://placeimg.com/140/140/any',
+                        }}
+                      />
+                      <Body>
+                        <Text numberOfLines={2}>
+                          Ilmu Pengetahuan Alam kelas XII SMA
+                        </Text>
+                        <Text note numberOfLines={1}>
+                          Redi ramdan
+                        </Text>
+                        <Text note>Ilmu pengetahuan</Text>
+                        <View style={{alignItems: 'flex-start'}}>
+                          <Rating
+                            readonly={true}
+                            ratingCount={5}
+                            startingValue={3.5}
+                            showRating={false}
+                            imageSize={20}
+                            style={{paddingVertical: 10}}
+                          />
+                        </View>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                </TouchableOpacity>
+              </View>
+            </Tab>
+            <Tab
+              heading="Premium"
+              tabStyle={{backgroundColor: 'white'}}
+              activeTabStyle={{backgroundColor: 'white'}}
+              textStyle={{color: 'black'}}
+              activeTextStyle={{color: 'black'}}>
+              <View style={{paddingTop: 20}}>
+                <TouchableOpacity onPress={() => {}}>
+                  <CardItem style={styles.carditem}>
+                    <Left style={styles.left}>
+                      <Thumbnail
+                        square
+                        style={styles.thumb}
+                        source={{
+                          uri: 'https://placeimg.com/140/140/any',
+                        }}
+                      />
+                      <Body>
+                        <Text numberOfLines={2}>
+                          Ilmu Pengetahuan Alam kelas XII SMA
+                        </Text>
+                        <Text note numberOfLines={1}>
+                          Redi ramdan
+                        </Text>
+                        <Text note>Ilmu pengetahuan</Text>
+                        <View style={{alignItems: 'flex-start'}}>
+                          <Rating
+                            readonly={true}
+                            ratingCount={5}
+                            startingValue={3.5}
+                            showRating={false}
+                            imageSize={20}
+                            style={{paddingVertical: 10}}
+                          />
+                        </View>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                  <CardItem style={styles.carditem}>
+                    <Left style={styles.left}>
+                      <Thumbnail
+                        square
+                        style={styles.thumb}
+                        source={{
+                          uri: 'https://placeimg.com/140/140/any',
+                        }}
+                      />
+                      <Body>
+                        <Text numberOfLines={2}>
+                          Ilmu Pengetahuan Alam kelas XII SMA
+                        </Text>
+                        <Text note numberOfLines={1}>
+                          Redi ramdan
+                        </Text>
+                        <Text note>Ilmu pengetahuan</Text>
+                        <View style={{alignItems: 'flex-start'}}>
+                          <Rating
+                            readonly={true}
+                            ratingCount={5}
+                            startingValue={3.5}
+                            showRating={false}
+                            imageSize={20}
+                            style={{paddingVertical: 10}}
+                          />
+                        </View>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                </TouchableOpacity>
+              </View>
+            </Tab>
+          </Tabs>
+        </View>
+      </Content>
+    </Container>
+  );
+};
+
+export default Home;
